814c5d755e675022174d41bfff381d2f72927efc
Eli <88557639+lishaduck@users.noreply.github.com>
2025-04-28 13:17:05 -0500

    feat: make .pnpmfile a bit quieter

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,5 +1,7 @@
 // @ts-check
 
+const VERBOSE = false;
+
 /// PNPM Typedefs.
 // From <https://github.com/pnpm/pnpm/blob/eeb76cd1d0a3133d71d96de5646e305f34474652/hooks/pnpmfile/src/Hooks.ts> & <https://github.com/pnpm/pnpm/blob/d500d9f12e8819bf2659abece126938fb3a9ac87/packages/types/src/package.ts>.
 
@@ -170,7 +172,7 @@ function readPackage(pkg, context) {
         newPkg.dependencies[name] = `npm:${nolyfill}@^1`;
 
         // Tell the user that the package has been nolyfilled.
-        context.log(`${name} => ${nolyfill}`);
+        if (VERBOSE) context.log(`${name} => ${nolyfill}`);
       }
     }
   }
